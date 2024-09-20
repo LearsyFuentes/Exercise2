6a7cbbf5101a88234f83bd214886e09189d5ca1d
israe <israe@Windows11-ISRA>
2024-09-20 20:43:00 +0200

    Excercise2

---
+++ Exercise2/src/exercise2/Exercise2.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println ("Hola, ¿cómo estás?");
+		System.out.println("Bien, gracias");
+
+	}
+
+}
